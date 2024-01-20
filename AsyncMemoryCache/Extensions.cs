29371bff9ad04dbd4edce721412ba47af863fc68
calledude <22471295+calledude@users.noreply.github.com>
2024-01-20 18:52:30 +0100

    Add ServiceCollection extensions

---
+++ AsyncMemoryCache/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AsyncMemoryCache;
+
+public static class Extensions
+{
+	public static IServiceCollection AddAsyncMemoryCache(this IServiceCollection services)
+		=> services
+			.AddSingleton(typeof(IAsyncMemoryCache<>), typeof(AsyncMemoryCache<>))
+			.AddSingleton(EvictionBehavior.Default);
+}
