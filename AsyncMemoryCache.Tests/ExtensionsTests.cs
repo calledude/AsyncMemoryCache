59074e0a10219b156f27a887125b1a9971eefcfd
calledude <22471295+calledude@users.noreply.github.com>
2024-01-21 09:04:48 +0100

    Add ExtensionsTests

---
+++ AsyncMemoryCache.Tests/ExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace AsyncMemoryCache.Tests;
+
+public class ExtensionsTests
+{
+	[Fact]
+	public void CanResolveAsyncMemoryCache()
+	{
+		var serviceProvider = new ServiceCollection()
+			.AddAsyncMemoryCache()
+			.BuildServiceProvider();
+
+		var asyncMemoryCache = serviceProvider.GetService<IAsyncMemoryCache<IAsyncDisposable>>();
+		Assert.NotNull(asyncMemoryCache);
+	}
+}
